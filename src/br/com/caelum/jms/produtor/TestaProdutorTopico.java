63a2a7283e6e85ae488dfcc5f0078ac9e6c22d5d
bruno-smv2@hotmail.com <Bruno Carvalho>
2016-07-24 13:00:22 -0300

    Criando repositorio

---
+++ src/br/com/caelum/jms/produtor/TestaProdutorTopico.java
@@ -0,0 +1,46 @@
+package br.com.caelum.jms.produtor;
+
+import java.io.StringWriter;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.xml.bind.JAXB;
+
+import br.com.caelum.jms.modelo.Pedido;
+import br.com.caelum.jms.modelo.PedidoFactory;
+
+public class TestaProdutorTopico {
+	public static void main(String[] args) throws Exception {
+		InitialContext context = new InitialContext();
+
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination loja = (Destination) context.lookup("loja");
+
+		MessageProducer producer = session.createProducer(loja);
+
+		Pedido pedido = new PedidoFactory().geraPedidoComValores();
+
+		// StringWriter writer = new StringWriter();
+		// JAXB.marshal(pedido, writer);
+		//
+		// String xml = writer.toString();
+
+		Message message = session.createObjectMessage(pedido);
+
+		message.setBooleanProperty("ebook", true);
+		producer.send(message);
+
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
